cd38e82d4d6adc7e8de99472ee2b7252a5486541
GrantDit <kiruha5854@mail.ru>
2023-05-19 20:49:21 +0300

    first test commit

---
+++ src/main/java/pro/sky/testhomework/TestHomeWorkApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.testhomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestHomeWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestHomeWorkApplication.class, args);
+    }
+
+}
---
+++ src/main/java/pro/sky/testhomework/model/User.java
@@ -0,0 +1,4 @@
+package pro.sky.testhomework.model;
+
+public class User {
+}
